02fc36e8fd192cec3b5c3957d7238429622780c7
MarecGents <marecgents_tao@sina.com>
2024-11-10 22:38:34 +0800

    test gitignore if work

---
+++ Test/MG-Mod-New/src/types/models/enums/PathTypes.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PathTypes = void 0;
+var PathTypes;
+(function (PathTypes) {
+    //Relative path to the main folder
+    PathTypes["TraderList"] = "trader/";
+    PathTypes["KeyList"] = "res/Keys/";
+    PathTypes["PriceList"] = "res/price/";
+    PathTypes["QuestList"] = "res/quest/";
+    PathTypes["SuperItemList"] = "db/SuperModItem/";
+    PathTypes["BrothersItemList"] = "db/BrothersItem/";
+    PathTypes["MGItemList"] = "db/MGItem/";
+    PathTypes["AssortItemList"] = "db/assort/";
+    PathTypes["ModConfigList"] = "res/config/";
+})(PathTypes || (exports.PathTypes = PathTypes = {}));
+//# sourceMappingURL=PathTypes.js.map
---
+++ Test/MG-Mod-New/src/types/utils/IClone.ts
@@ -0,0 +1,25 @@
+import {DependencyContainer} from "tsyringe";
+import {ImporterUtil} from "@spt/utils/ImporterUtil";
+import {JsonUtil} from "@spt/utils/JsonUtil";
+import {ICloner} from "@spt/utils/cloners/ICloner";
+
+export class IClone implements ICloner {
+    private mod: any;
+
+    constructor(mod) {
+        this.mod = mod;
+    }
+
+    clone<T>(value: T) {
+        const JmporterUtil = this.mod.container.resolve<ImporterUtil>("ImporterUtil");
+        const JsonUtil = this.mod.container.resolve<JsonUtil>('JsonUtil');
+        if (typeof (value) === "object" && value !== null) {
+            return JsonUtil.clone(value);
+        } else if (typeof value === "string") {
+            return JsonUtil.clone(JmporterUtil.loadRecursive(value));
+        }
+
+    }
+}
+
+.0
