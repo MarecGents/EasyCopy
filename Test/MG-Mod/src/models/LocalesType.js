03cd4412571b65f042a4efe7540d44a483da01fb
MarecGents <marecgents_tao@sina.com>
2024-11-10 22:44:27 +0800

    remove no used folder

--- Test/MG-Mod/src/models/LocalesType.js
+++
@@ -1,8 +0,0 @@
-class LocalesType {
-    static FullName = "name";
-    static FirstName = "surname";
-    static Nickname = "nickname";
-    static Location = "location";
-}
-
-module.exports = LocalesType
