3b8f855b424b64d470c7c31b3fcefdfbee6cc037
MarecGents <marecgents_tao@sina.com>
2025-05-07 23:04:47 +0800

    clear project code to rewrite

--- Main/App.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-
-from src import GUI
-
-
-if __name__ == "__main__":
-	nowPath = str(pathlib.Path().absolute()) + "\\"
-	mainloop = GUI.CutTools(exePath=nowPath).root.mainloop()
-	pass
