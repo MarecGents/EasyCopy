ab91fb703faa62788302357c333fedc67647e2fc
MarecGents <marecgents_tao@sina.com>
2025-09-04 12:40:47 +0800

    20250904 reflash

--- Code/Main/src/GUI.py
+++ Code/Main/src/GUI.py
@@ -1,2 +1,17 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
 import ttkbootstrap as ttks
+from ttkbootstrap.constants import *
+import win32api
+# from threading import Thread
+from ttkbootstrap.scrolled import ScrolledFrame
+from ttkbootstrap.tooltip import ToolTip
+
+class Application:
+    def __init__(self):
+        self.root = ttks.Window()
+
+        pass
+    pass
